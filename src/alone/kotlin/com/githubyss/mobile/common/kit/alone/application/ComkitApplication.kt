9ef9e36687637914c6e060c41fbd6766f3b62bb7
ace <hotyss@hotmail.com>
2021-12-27 11:47:26 +0800

    1）更新混淆文件；2）更新alone独立运行代码；3）更新语音识别管理类

--- src/alone/kotlin/com/githubyss/mobile/common/kit/alone/application/ComkitApplication.kt
+++ src/alone/kotlin/com/githubyss/mobile/common/kit/alone/application/ComkitApplication.kt
@@ -1,33 +1,34 @@
-// package com.githubyss.mobile.common.kit.application
-//
-// import kotlin.properties.Delegates
-//
-//
-// /**
-//  * ComkitApplication
-//  *
-//  * @author Ace Yan
-//  * @github githubyss
-//  * @createdTime 2021/08/24 19:34:38
-//  */
-// class ComkitApplication : BaseApplication() {
-//
-//     /** ********** ********** ********** Properties ********** ********** ********** */
-//
-//     companion object {
-//         var instance: ComkitApplication by Delegates.notNull()
-//             private set
-//
-//         private val TAG: String = ComkitApplication::class.java.simpleName
-//     }
-//
-//
-//     /** ********** ********** ********** Override ********** ********** ********** */
-//
-//     override fun onCreate() {
-//         super.onCreate()
-//         instance = this
-//
-//         initARouter(instance)
-//     }
-// }
+package com.githubyss.mobile.common.kit.alone.application
+
+import com.githubyss.mobile.common.ui.application.BaseApplication
+import kotlin.properties.Delegates
+
+
+/**
+ * ComkitApplication
+ *
+ * @author Ace Yan
+ * @github githubyss
+ * @createdTime 2021/08/24 19:34:38
+ */
+class ComkitApplication : BaseApplication() {
+
+    /** ********** ********** ********** Properties ********** ********** ********** */
+
+    companion object {
+        var instance: ComkitApplication by Delegates.notNull()
+            private set
+
+        private val TAG: String = ComkitApplication::class.java.simpleName
+    }
+
+
+    /** ********** ********** ********** Override ********** ********** ********** */
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+
+        initARouter(instance)
+    }
+}
--- src/main/kotlin/com/githubyss/mobile/common/kit/manager/speech_recognition/SpeechRecognitionManager.kt
+++ src/main/kotlin/com/githubyss/mobile/common/kit/manager/speech_recognition/SpeechRecognitionManager.kt
@@ -1,8 +1,10 @@
 package com.githubyss.mobile.common.kit.manager.speech_recognition
 
+import android.app.Activity
 import android.content.Context
-import com.iflytek.cloud.SpeechConstant
-import com.iflytek.cloud.SpeechUtility
+import com.githubyss.mobile.common.kit.AceTemp
+import com.githubyss.mobile.common.kit.util.LogUtils
+import com.iflytek.cloud.*
 
 
 /**
@@ -15,14 +17,38 @@ import com.iflytek.cloud.SpeechUtility
  */
 object SpeechRecognitionManager {
 
-    /** ********** ********** ********** Properties ********** ********** ********** */
+    /** ****************************** Properties ****************************** */
+
+    private val TAG: String = AceTemp::class.java.simpleName
+    var iat: SpeechRecognizer? = null
+    var textUnderstander: TextUnderstander? = null
+
+    /** ****************************** Functions ****************************** */
 
     fun initSdk(context: Context, appId: String = "59898a55") {
         // 初始化科大讯飞 SDK
-        SpeechUtility.createUtility(context, SpeechConstant.APPID.toString() + "=$appId")
+        SpeechUtility.createUtility(context, SpeechConstant.APPID + "=$appId")
+    }
+
+    fun configSDK(activity: Activity, listener: InitListener = textUdrInitListener) {
+        // 1.创建SpeechRecognizer对象，第二个参数：本地听写时传InitListener
+        iat = SpeechRecognizer.createRecognizer(activity, null)
+
+        // 2.设置听写参数，详见《科大讯飞MSC API手册(Android)》SpeechConstant类
+        // 初始化文本到语义
+        textUnderstander = TextUnderstander.createTextUnderstander(activity, listener)
     }
 
 
-    /** ********** ********** ********** Functions ********** ********** ********** */
+    /** ****************************** Implementations ****************************** */
 
+    /**
+     * 初始化监听器（文本到语义）。
+     */
+    private val textUdrInitListener = InitListener { code ->
+        if (code != ErrorCode.SUCCESS) {
+            LogUtils.d(TAG, "初始化失败，错误码：$code")
+            // ToastUtil.showMessage("初始化失败,错误码：$code")
+        }
+    }
 }
