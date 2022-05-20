3bd6d65eb25222330f36c186d3ef5098a1aa2d9f
ace <hotyss@hotmail.com>
2022-05-20 11:19:41 +0800

    1）修改包配置方式，简化build.gradle文件；2）移出混淆文件到工程目录；3）引入Kotlin测试包。

--- src/main/kotlin/com/githubyss/mobile/common/kit/binding/binding_adapter/ImageViewBindingAdapter.java
+++ src/main/kotlin/com/githubyss/mobile/common/kit/binding/binding_adapter/ImageViewBindingAdapter.java
@@ -1,21 +1,28 @@
-// package com.githubyss.mobile.common.kit.mvvm.bindingadapter;
-//
-// import android.widget.ImageView;
-//
-// import com.githubyss.mobile.common.kit.glide.GlideUtils;
-//
-// import androidx.databinding.BindingAdapter;
-//
-//
-// public class ImageViewBindingAdapter {
-//
-//     @BindingAdapter({"path"})
-//     public static void loadImage(ImageView view, Object path) {
-//         GlideUtils.INSTANCE.loadImage(view, view, path, null, null);
-//     }
-//
-//     @BindingAdapter(value = {"path", "placeholder", "error"}, requireAll = false)
-//     public static void loadImage(ImageView view, Object path, Object placeholder, Object error) {
-//         GlideUtils.INSTANCE.loadImage(view, view, path, placeholder, error);
-//     }
-// }
+package com.githubyss.mobile.common.kit.mvvm.bindingadapter;
+
+import android.widget.ImageView;
+
+import com.githubyss.mobile.common.kit.glide.GlideUtils;
+
+import androidx.databinding.BindingAdapter;
+
+
+/**
+ * ImageViewBindingAdapter
+ *
+ * @author Ace Yan
+ * @github githubyss
+ * @createdTime 2022/05/19 11:26:05
+ */
+public class ImageViewBindingAdapter {
+
+    @BindingAdapter({"path"})
+    public static void loadImage(ImageView view, Object path) {
+        GlideUtils.INSTANCE.loadImage(view, view, path, null, null);
+    }
+
+    @BindingAdapter(value = {"path", "placeholder", "error"}, requireAll = false)
+    public static void loadImage(ImageView view, Object path, Object placeholder, Object error) {
+        GlideUtils.INSTANCE.loadImage(view, view, path, placeholder, error);
+    }
+}
